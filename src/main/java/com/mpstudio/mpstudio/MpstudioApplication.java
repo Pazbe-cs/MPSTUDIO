2a5183f6c1654cc73665c21672a35c382de7a929
Paz <pazbe98@icloud.com>
2026-03-31 15:47:27 -0300

    feat: agregar proyecto Spring Boot MPStudio

---
+++ src/main/java/com/mpstudio/mpstudio/MpstudioApplication.java
@@ -0,0 +1,13 @@
+package com.mpstudio.mpstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpstudio/mpstudio/MpstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpstudio.mpstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
